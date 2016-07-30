39388aaeb6082cb544ebd79828ae305dd533e27f
Frank Capraro <Frank@Franks-MBP.fios-router.home>
2016-07-29 20:04:59 -0400

    html completed

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,16 +1,8 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
+// Dependencies
+// =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
-
-
-
-
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -18,21 +10,14 @@ var path = require('path');
 // ==============================================================================
 
 var app = express(); // Tells node that we are creating an "express" server
-var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+var PORT = 3000; // Sets an initial port. We'll use this later in our listener
 
 
 // BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-
-
-
-
-
 
 // ================================================================================
 // ROUTER
@@ -40,14 +25,17 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
 // ================================================================================
 
+app.get("/",function(req,res){
 
+	res.sendFile(path.join(__dirname, '/app/public/home.html'));
 
+});
 
+app.get("/survey", function(req,res){
 
+	res.sendFile(path.join(__dirname, "/app/public/survey.html"));
 
-
-
-
+});
 
 // ==============================================================================
 // LISTENER
